c5c8388f2c5a11cec1fbd4a57b305f5de91ce933
k.zyryanov <KirillVar1@mail.ru>
2022-05-11 01:57:35 +0500

    i've done smth

---
+++ laravel/app/Models/Meeting.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class Meeting extends Authenticatable
+{
+    use HasApiTokens, HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'status',
+        'meeting_time',
+        'show_time',
+        'description',
+        'tags',
+        'participants_need',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'participants_have' => 0,
+    ];
+}
---
+++ laravel/database/migrations/2022_05_10_194532_create_meetings_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMeetingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('meetings', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->integer('status');
+            $table->dateTime('meeting_time');
+            $table->dateTime('show_time');
+            $table->string('name');
+            $table->string('description');
+            $table->string('tags');
+            $table->smallinteger('participants_need');
+            $table->smallinteger('participants_have');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('meetings');
+    }
+}
